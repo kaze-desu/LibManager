065b5777130579ec09813499a14501f3a71a40da
Xiaohuo-Thinkbook <1220026920@student.must.edu.mo>
2023-04-29 23:37:09 +0800

    修正： 修正AddDao类中SQL语句的错误

--- src/com/xiaohuo/libmanager/dao/AddDao.java
+++ src/com/xiaohuo/libmanager/dao/AddDao.java
@@ -20,8 +20,10 @@ import java.util.List;
 
 public class AddDao
 {
+    public static final String COPY_RIGHT = "CopyRight";
     private Connection conn = null;
     private PreparedStatement pstmt = null;
+    private ResultSet rs = null;
     private final String table = "BookList";
     private boolean init = false;
 
@@ -36,7 +38,7 @@ public class AddDao
         //Check if the table exists.
         List<Throwable> exceptions = new ArrayList<>();
         conn = DatabaseConnect.connect();
-        String sql = "CREATE TABLE IF NOT EXISTS BookList (BookID INT PRIMARY KEY AUTO_INCREMENT,Tittle VARCHAR(255) NOT NULL,Author VARCHAR(255) NOT NULL,Publisher VARCHAR(255) NOT NULL,Category VARCHAR(255) NOT NULL)";
+        String sql = "CREATE TABLE IF NOT EXISTS BookList (BookID INT PRIMARY KEY AUTO_INCREMENT,Type VARCHAR(255) NOT NULL ,Tittle VARCHAR(255) NOT NULL,Author VARCHAR(255) NOT NULL,Publisher VARCHAR(255) NOT NULL,Category VARCHAR(255) NOT NULL)";
         try
         {
             //Create table if not exists.
@@ -64,15 +66,16 @@ public class AddDao
 
     /**
      * Add books to database in general.
-     * @param conn Connection session
      * @param columnSql SQL for checking if the table has the column.
      * @param columnList List of columns.
      * @param bookSql SQL for adding books.
      * @param bookList List of books.
      * @throws CollectionException Exception thrown when there is any error.
      */
-    public void add(Connection conn,String columnSql,ArrayList<String> columnList,String bookSql,ArrayList<String> bookList) throws CollectionException
+    public void add(String columnSql,ArrayList<String> columnList,String bookSql,ArrayList<String> bookList) throws CollectionException
     {
+        //TODO 考虑如何使用JDBC Batch将语句一并提交到数据库，减少调用，增加效率
+        conn = DatabaseConnect.connect();
         //Check the test mode flag.
         boolean testMode = new Init().getTestMode();
         List<Throwable> exceptions = new ArrayList<>();
@@ -83,36 +86,39 @@ public class AddDao
             throw new CollectionException(exceptions);
         }
         //Check if the table has the column.
+       //TODO 本处数量判断有问题，需要修改,，考虑通过检查，将不存在的列的位置(i)存储
+        int count = 0;
         try
         {
             pstmt = conn.prepareStatement(columnSql);
-            for (int i = 1; i < columnList.size(); i++)
+            for (int i = 0; i < columnList.size(); i++)
             {
-                pstmt.setString(1,table);
-                pstmt.setString(i+1,columnList.get(i));
+                pstmt.setString(1,columnList.get(i));
+                pstmt.addBatch();
+                if(testMode){System.out.println("try to check column: "+ columnList.get(i));}
             }
+            int[] result = pstmt.executeBatch();
+
+            //rs = pstmt.executeQuery();
         }
         catch (SQLException e)
         {
             e.printStackTrace();
             exceptions.add(e);
         }
+
         //If not, add the column.
         try
         {
-            if(pstmt.executeUpdate()<1)
+            if(count<columnList.size())
             {
-                String sql = "ALTER TABLE ? ADD ? VARCHAR(255) NOT NULL";
-                for (int i = 1; i < columnList.size(); i++)
+                for (int i = 0; i < columnList.size(); i++)
                 {
+                    String addSql = "ALTER TABLE "+ table +" ADD "+columnList.get(i)+" VARCHAR(255)";
                     try
                     {
-                        pstmt = conn.prepareStatement(sql);
-                        pstmt.setString(1,table);
-                        pstmt.setString(2,columnList.get(i));
-                        int update = pstmt.executeUpdate();
-                        //Check if the test mode is on.
-                        if(testMode){System.out.println("Ran columnSQL and updated: "+update+" rows");}
+                        pstmt = conn.prepareStatement(addSql);
+                        pstmt.addBatch();
                     }
                     catch (SQLException e)
                     {
@@ -120,6 +126,9 @@ public class AddDao
                         exceptions.add(e);
                     }
                 }
+                int[] result = pstmt.executeBatch();
+                //Check if the test mode is on.
+                if(testMode){System.out.println("Ran columnSQL and updated: "+result.length+" rows");}
             }
         }
         catch (SQLException e)
@@ -131,10 +140,11 @@ public class AddDao
         try
         {
             pstmt = conn.prepareStatement(bookSql);
-            for (int i = 1; i < bookList.size(); i++)
+
+            for (int i = 0; i < bookList.size(); i++)
             {
-                pstmt.setString(1,table);
                 pstmt.setString(i+1,bookList.get(i));
+                if(testMode){System.out.println("Adding bookList :"+bookList.get(i));}
             }
             int update = pstmt.executeUpdate();
             //Check if the test mode is on.
@@ -145,16 +155,12 @@ public class AddDao
             e.printStackTrace();
             exceptions.add(e);
         }
-        finally
-        {
-            //Close connection.
-            DatabaseClose.close(pstmt,conn);
-        }
-        //Throw exceptions if any.
         if (exceptions.size() > 0)
         {
             throw new CollectionException(exceptions);
         }
+        //Close connection.
+        DatabaseClose.close(pstmt,conn);
     }
 
     /**
@@ -166,22 +172,20 @@ public class AddDao
     public void addBook(ArrayList<BaseBooks> booksInfo) throws CollectionException
     {
         ArrayList<String>columnList = new ArrayList<>();
-        //Start a connect session.
-        conn = DatabaseConnect.connect();
 
         //Check if the table has the column Isbn.
-        String columnSql = "SHOW COLUMNS FROM ? LIKE '?'";
-        columnList.add("Issn");
+        String columnSql = "SHOW COLUMNS FROM "+table+" LIKE ?";
+        columnList.add("Isbn");
 
         //Add books to the table.
-        String bookSql = "INSERT INTO ? (Tittle,Author,Category,Isbn) VALUES (?,?,?,?)";
+        String bookSql = "INSERT INTO "+table+" (Type,Tittle,Author,Publisher,Category,Isbn) VALUES (?,?,?,?,?,?)";
         for (BaseBooks baseBooks : booksInfo)
         {
             //Cast the object to Book, because we need the Isbn which only in book class.
             Book book = (Book) baseBooks;
             ArrayList<String> bookList = book.getBookInfo();
             //Call the add method.
-            add(conn,columnSql,columnList,bookSql,bookList);
+            add(columnSql,columnList,bookSql,bookList);
         }
     }
 
@@ -194,45 +198,43 @@ public class AddDao
     public void addJournal(ArrayList<BaseBooks>journalInfo)throws CollectionException
     {
         ArrayList<String>columnList = new ArrayList<>();
-        //Start a connect session.
-        conn = DatabaseConnect.connect();
 
         //Check if the table has the column Issn.
-        String columnSql = "SHOW COLUMNS FROM ? LIKE '?'";
+        String columnSql = "SHOW COLUMNS FROM "+table+" LIKE ?";
         columnList.add("Issn");
 
         //Add journals to the table.
-        String journalSql = "INSERT INTO ? (Tittle,Author,Category,Issn) VALUES (?,?,?,?)";
+        String journalSql = "INSERT INTO "+table+" (Type,Tittle,Author,Publisher,Category,Issn) VALUES (?,?,?,?,?,?)";
         for (BaseBooks baseBooks : journalInfo)
         {
             Journal journal = (Journal) baseBooks;
             ArrayList<String> journalList = journal.getBookInfo();
-            add(conn,columnSql,columnList,journalSql,journalList);
+            add(columnSql,columnList,journalSql,journalList);
         }
     }
 
     /**
      * Add newspaper to database.
+     * @Type: Newspaper
      * @param newspaperInfo List of newspapers.
      * @throws CollectionException Exception thrown when there is any error.
      */
     public void addNewspaper(ArrayList<BaseBooks>newspaperInfo)throws CollectionException
     {
         ArrayList<String>columnList = new ArrayList<>();
-        conn = DatabaseConnect.connect();
 
         //Check if the table has the column Issn and Copyright.
-        String columnSql = "SHOW COLUMNS FROM ? LIKE '?' AND '?'";
+        String columnSql = "SHOW COLUMNS FROM "+table+" LIKE ?";
         columnList.add("Issn");
-        columnList.add("CopyRight");
+        columnList.add("COPY_RIGHT");
 
         //Add books to the table.
-        String journalSql = "INSERT INTO ? (Tittle,Author,Category,Issn,CopyRight) VALUES (?,?,?,?,?)";
+        String newspaperSql = "INSERT INTO "+table+" (Type,Tittle,Author,Publisher,Category,Issn,CopyRight) VALUES (?,?,?,?,?,?,?)";
         for (BaseBooks baseBooks : newspaperInfo)
         {
             Newspaper newspaper = (Newspaper) baseBooks;
             ArrayList<String> newspaperList = newspaper.getBookInfo();
-            add(conn,columnSql,columnList,journalSql,newspaperList);
+            add(columnSql,columnList,newspaperSql,newspaperList);
         }
     }
 }
