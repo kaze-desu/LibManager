ff5c3b92ef7a976a37bd459c7065f18d8c4ee394
小火 <1178035823@qq.com>
2023-05-25 16:28:27 +0900

    开始编写SearchController

--- src/main/java/com/xiaohuo/libmanagergui/ApplicationMain.java
+++ src/main/java/com/xiaohuo/libmanagergui/ApplicationMain.java
@@ -5,7 +5,11 @@ import com.xiaohuo.libmanagergui.controller.WelcomeScene;
 import io.vproxy.vfx.ui.scene.*;
 import io.vproxy.vfx.ui.stage.VStage;
 
+import io.vproxy.vfx.ui.stage.VStageInitParams;
+import io.vproxy.vfx.util.FXUtils;
 import javafx.application.Application;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.util.ArrayList;
@@ -21,14 +25,21 @@ public class ApplicationMain extends Application
     @Override
     public void start(Stage primaryStage)
     {
-        VStage stage = new VStage(primaryStage);
-        var welcome = new WelcomeScene();
-        stage.getRootSceneGroup().addScene(welcome,VSceneHideMethod.TO_LEFT);
-        stage.getRootSceneGroup().show(welcome,VSceneShowMethod.FADE_IN);
+        scenes.add(new WelcomeScene());
+        var vstageInit = new VStageInitParams();
+        VStage stage = new VStage(primaryStage, vstageInit);
+        sceneGroup = new VSceneGroup(scenes.get(0));
+        sceneGroup.show(scenes.get(0),VSceneShowMethod.FROM_RIGHT);
+
+        var box = new HBox();
+        box.getChildren().add(sceneGroup.getNode());
+        stage.getInitialScene().getContentPane().getChildren().add(box);
+        FXUtils.observeWidthHeightCenter(stage.getInitialScene().getNode(), box);
         stage.setTitle("图书管理系统");
         stage.getStage().setWidth(1280);
         stage.getStage().setHeight(800);
         stage.getStage().centerOnScreen();
         stage.show();
+
     }
 }
